0aaae40f1c422416196a12869561d374dcf6babd
wsagv <wsagv2@gmail.com>
2020-07-12 23:20:42 -0300

    endereçamento

--- script.js
+++ script.js
@@ -52,12 +52,12 @@ function relogio(){
         texto.innerText = "Tenha um ótimo dia!"
     } else if (hor >= 12 && hor < 18) {
         // Boa tarde
-        img.src = './tarde.png'
+        img.src = 'tarde.png'
         document.body.style.background = '#f1f1f1'
         texto.innerText = "Tenha uma ótima tarde!"
     } else {
         // Boa noite
-        img.src = './noite.png'
+        img.src = 'noite.png'
         document.body.style.background = '#317f80'
         texto.style.color = '#008080'
         titulo.style.color = '#fff'
